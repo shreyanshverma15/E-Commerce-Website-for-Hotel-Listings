b3e3b88e5a4b49f387dd8f324dc47686c00c2f77
Shreyansh Verma <127296619+shreyanshverma15@users.noreply.github.com>
2025-07-15 22:24:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -111,6 +111,9 @@ app.use((req, res, next) =>
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 app.use("/", userRouter);
 
 // app.get("/testListing", async (req, res) =>
